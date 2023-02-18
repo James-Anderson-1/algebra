9b87eb9d8104a6d6e28a9a23d04f28919ecac898
james.anderson <anderson.james_@outlook.com>
2023-02-18 11:46:34 +1030

    include content list and corresponding entry apply buttons

--- source/algebra_server/app.py
+++ source/algebra_server/app.py
@@ -3,9 +3,37 @@ from flask import render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        "id": 1,
+        "title": "Data Analyst",
+        "Location": "Bengaluru, India",
+        "salary": "Rs. 15 0000"
+    },
+    {
+        "id": 2,
+        "title": "PHD",
+        "Location": "Canberra, Aus",
+        "salary": "Rs. 15 0000"
+    },
+    {
+        "id": 3,
+        "title": "Professor",
+        "Location": "Adelaida, Aus",
+        "salary": "$ 12 000"
+    },
+    {
+        "id": 3,
+        "title": "Professor",
+        "Location": "Adelaida, Aus",
+    }
+]
+
+COMPANY_NAME = "Algebra Phone Book Test"
+
 @app.route("/")
 def root_path():
-    return render_template("home.html")
+    return render_template("home.html",jobs=JOBS, company_name = COMPANY_NAME)
 
 
 if __name__ == "__main__":
